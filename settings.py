4dfb4afc1133f9cd43a63397d10d19fc3821f18d
loque36 <loque36@gmail.com>
2021-10-21 14:24:59 +0500

    Steps to create game project!

---
+++ settings.py
@@ -0,0 +1,10 @@
+
+class Setting():
+	"""Класс для хранения всех настроек игры"""
+
+	def __init__(self):
+		"""Инициализирует настройки игрыю"""
+		# Параметры экрана
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (80,230,200)
